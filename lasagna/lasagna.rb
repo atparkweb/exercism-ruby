4d6ae9803ff29a0edeecbf9a3dc1bc66b4b3bde7
Andrew Park <atpark333@gmail.com>
2022-03-30 23:36:52 +0900

    Add expected minutes in oven constant

--- lasagna/lasagna.rb
+++ lasagna/lasagna.rb
@@ -1,16 +1,14 @@
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
-  MINUTES_PER_LAYER = 2
-
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+    raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
   end
 
   def preparation_time_in_minutes(layers)
-    layers * MINUTES_PER_LAYER
+    raise 'Please implement the Lasagna#preparation_time_in_minutes method'
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+    raise 'Please implement the Lasagna#total_time_in_minutes method'
   end
 end
