bbcfdb9a7ea00aa22b00f9ddab1022ea58355123
Andrew Park <atpark333@gmail.com>
2022-03-30 23:37:33 +0900

    Implement remaining_minutes_in_oven method

--- lasagna/lasagna.rb
+++ lasagna/lasagna.rb
@@ -1,7 +1,8 @@
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
+
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
