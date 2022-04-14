c50e9970614e86b1125b2daa84a03661e7eae499
Andrew Park <atpark333@gmail.com>
2022-04-14 21:05:23 +0900

    Solve assembly line

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -1,13 +1,20 @@
 class AssemblyLine
+  UNITS_PER_HOUR = 221
+
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    success_rate = 1.00 if @speed.between?(1, 5)
+    success_rate = 0.90 if @speed.between?(5, 9)
+    success_rate = 0.80 if @speed == 9
+    success_rate = 0.77 if @speed == 10
+
+    success_rate * @speed * UNITS_PER_HOUR
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).floor
   end
 end
